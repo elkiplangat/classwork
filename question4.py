cc502afef0c856caa054f5cfd27bc89d65d64263
elkiplangat <langatkip23@yahoo.com>
2021-03-25 22:05:50 +0300

    answer question4

--- question4.py
+++ question4.py
@@ -1,36 +1,4 @@
 
-def solve_maze(m,s,g):
-
-    if not m:
-        return
-    elif s == g:
-        print(f"Youre done! you're at: {s}")
-        return
-    else:
-        print(f"S: {s}")
-        #print(m[s[0]][s[1]])
-        if m[s[0]][s[1]] == '0':
-            print("Yaaay!")
-            print(f"fucking {m}")
-            east = (s[0], s[1] + 1)
-            s = east
-            solve_maze(m,s,g)
-            # south = (s[0]-1, s[1])
-            # north = (s[0]+1, s[1])
-            # west = (s[0], s[1]-1)
-            # east = (s[0], s[1]+1)
-            # print(f"bottom : {south}")
-            # print(f"north : {north}")
-            # print(f"west : {west}")
-            # print(f"right : {east}")
-
-        else:
-            print("No through way")
-
-
-        #return
-    ...
-
 with open("Maze1.txt", "r") as f:
     lines = f.readlines()
     #print(lines)
@@ -38,4 +6,40 @@ with open("Maze1.txt", "r") as f:
     #print(array[1][1])
     #solve_maze(array,(0,0), ())
 example = [line.split() for line in ['0 0 0 0 0 1 0 0', '1 0 1 1 0 0 0 0', '1 0 0 1 0 1 0 1', '0 1 0 0 0 1 0 0', '0 0 0 0 1 1 0 1','0 0 1 0 0 0 0 0' ]]
+#solve_maze(example, (0,0), (5,7))
+path = []
+allowed_moves = [(0,1), (0,-1), (1,0), (-1,0)] 
+'''
+allowed moves: 0,1 when you add 1 to the column, you move right
+                0,-1 move left
+                1,0 when you add one to the row you move down
+                -1,0 move up
+'''
+visited = [ (0,0)]
+
+def solve_maze(m, s,g):
+    
+    
+    if (s[0] == g[0]) and (s[1]==g[1]):
+        #print path
+        print("found ya!")
+        print(visited)
+        return
+    else:
+        for i in range(len(allowed_moves)): # iterate through all movements
+            new = (s[0]+allowed_moves[i][0], s[1]+allowed_moves[i][1])
+            if (new[0] >= 0) and (new[0]<len(m)) and (new[1]>=0) and (new[1]<len(m[0])) and ((m[new[0]][new[1]])=='0') and (new not in visited):
+                visited.append(new)              
+                # if (new[0] == g[0]) and (new[1]==g[1]):
+                #     print("Solved by now")
+                #     break
+                solve_maze(m, new, g)
+            elif new in visited:
+                continue
+     
+            
+              
+
 solve_maze(example, (0,0), (5,7))
+
+#print(visited)
